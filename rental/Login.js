7a0dc195bdbfe78924dcc14b9ea6768992a91247
Mohanaselvan <101503204+MohanaselvanDV@users.noreply.github.com>
2023-09-29 20:03:47 +0530

    Add files via upload

--- rental/Login.js
+++ rental/Login.js
@@ -1,118 +1,58 @@
-import React, { useState } from "react";
-import './Login.css';
+import React, { useState } from 'react';
+// import axios from 'axios';
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-const Login = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [contact, setContact] = useState('');
-    const [address, setAddress] = useState('');
-    const [idproof, setId] = useState('');
-    const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/login', {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body:JSON.stringify({customer_mail:email, customer_password:password},)
+       
+      });
+     
+      const data =await response.json();
+      console.log(data);
 
-    const handleName = (e) => {
-        setName(e.target.value);
-        setSubmitted(false);
-    };
+    } catch (error) {
+      console.error("Error",error) // Handle login errors
+    }
+  };
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        setSubmitted(false);
-    };
-
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-        setSubmitted(false);
-    };
-    const handleContact = (e) => {
-        setContact(e.target.value);
-        setSubmitted(false);
-    };
-    const handleAddress = (e) => {
-        setAddress(e.target.value);
-        setSubmitted(false);
-    };
-    const handleId = (e) => {
-        setId(e.target.value);
-        setSubmitted(false);
-    };
-
-    const successMessage = () => {
-        return (
-            <div
-                className="success"
-                style={{
-                    display: submitted ? '' : 'none',
-                }}>
-                <h1>User {name} successfully registered!!</h1>
-            </div>
-        );
-    };
-
-
-    const errorMessage = () => {
-        return (
-            <div
-                className="error"
-                style={{
-                    display: error ? '' : 'none',
-                }}>
-                <h1>Please enter all the fields</h1>
-            </div>
-        );
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (name === '' || email === '' || password === '' || contact === '' || address === '' || idproof === '') {
-            setError(true);
-        } else {
-            setSubmitted(true);
-            setError(false);
-        }
-    };
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="text"> Sign Up </div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs" align = "center">
-                <form>
-
-                    <label className="label">Name</label>
-                    <input onChange={handleName} className="input"
-                        value={name} type="text" />
-
-                    <label className="label">Email</label>
-                    <input onChange={handleEmail} className="input"
-                        value={email} type="email" />
-
-                    <label className="label">Password</label>
-                    <input onChange={handlePassword} className="input"
-                        value={password} type="password" />
-
-                    <label className="label">Contact</label>
-                    <input onChange={handleContact} className="input"
-                        value={contact} type="tel" />
-
-                    <label className="label">Address</label>
-                    <input onChange={handleAddress} className="input"
-                        value={address} type="text" />
-
-                    <label className="label">ID Proof</label>
-                    <input onChange={handleId} className="input"
-                        value={idproof} type="file" />
-                    <button onClick={handleSubmit} className="btn" type="submit">
-                        Submit
-                    </button>
-                </form>
-            </div>
+   return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
-
-    );
-
+        <div>
+          <label>Password:</label>
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
-
-export default Login 
+export default LoginPage;
